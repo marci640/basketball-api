546237fa6cc26a5d8a2d2e48b7e354b37636323b
Unknown <marcifey@gmail.com>
2018-03-02 15:43:35 -0800

    scanned pdf and split teams into elements in an array

--- app/controllers/api/v1/conferences_controller.rb
+++ app/controllers/api/v1/conferences_controller.rb
@@ -1,55 +1,57 @@
 class Api::V1::ConferencesController < ApplicationController
-  
+
   def index
     @conferences = Conference.all
     first_four = ["Mt St Mary's", "New Orleans", "Providence", "USC", "N.C. Central", "UC Davis", "Kansas St.", "Wake Forest"]
-
     require 'open-uri'
     io = open('http://sports.cbsimg.net/images/collegebasketball/ncaa-tournament/brackets/printable/cbs-sports-2017.pdf'.to_s)
     reader = PDF::Reader.new(io)
-    reader.pages.each do |page|
-      string = page.text
-      
-      @conferences.each do |conference|
-        conference.teams.each do |team|
 
-          if first_four.include?(team.college_name)
-            team.update(first_four: true)
-          end
+    string = reader.pages[0].text
+    array = string.gsub!(/(\d+)|([ ]{2,})|(\n)/, ',').split(',').map(&:strip).reject { |string| string.empty? }
+    # Turn all integers, spaces (more than 2), and new lines into commas. Split on commas. Remove extra spaces around strings, and remove all empty strings from array.
+    scorecard_hash = array.each_with_object(Hash.new(0)) { |team,count| count[team] += 1 }
 
-          if team.update_first_round(string) == true
-            team.update( first_round: true)
-          end 
+    @conferences.each do |conference|
+      conference.teams.each do |team|
 
-          if team.update_second_round(string) == true
-            team.update(second_round: true)
-          end 
+        if first_four.include?(team.college_name)
+          scorecard_hash[team.college_name] = (scorecard_hash[team.college_name]) -1
+          team.update(first_four: true)
+        end
 
-          if team.update_regional_semifinals(string) == true
-            team.update(regional_semifinals: true)
-          end
+        if scorecard_hash[team.college_name] >=1
+          team.update(first_round: true)
+        end
 
-          if team.update_regional_finals(string) == true
-            team.update(regional_finals: true)
-          end
+        if scorecard_hash[team.college_name] >=2
+          team.update(second_round: true)
+        end
 
-          if team.update_final_four(string) == true
-            team.update(final_four: true)
-          end
+        if scorecard_hash[team.college_name] >=3
+          team.update(regional_semifinals: true)
+        end
 
-          if team.update_national_championship(string) == true
-            team.update(national_championship: true)
-          end
+        if scorecard_hash[team.college_name] >=4
+          team.update(regional_finals: true)
+        end
 
-          if team.update_winner(string) == true
-            team.update(winner: true)
-          end
+        if scorecard_hash[team.college_name] >=5
+          team.update(final_four: true)
+        end
 
-        end 
+        if scorecard_hash[team.college_name] >=6
+          team.update(national_championship: true)
+        end
+
+        if scorecard_hash[team.college_name] >=7
+          team.update(winner: true)
+        end
+
       end
-
     end
+
     render 'index.json.jbuilder'
-  end 
+  end
 
 end
